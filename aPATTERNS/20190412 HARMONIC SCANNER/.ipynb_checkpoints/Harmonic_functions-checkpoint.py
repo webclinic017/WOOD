8c3ce068237039d734990a78a2fe97b9e7e7f320
LumberJack <tapuz@free.fr>
2021-06-10 12:36:13 +0200

    Ajout dans aPATTERNS

---
+++ aPATTERNS/20190412 HARMONIC SCANNER/.ipynb_checkpoints/Harmonic_functions-checkpoint.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from scipy.signal import argrelextrema
+def peak_detect(price,order=30):
+    max_idx = list(argrelextrema(price,np.greater,order=order)[0])
+    min_idx = list(argrelextrema(price,np.less,order=order)[0])
+    idx = max_idx + min_idx + [len(price)-1]
+    idx.sort()
+    current_idx = idx[-5:]
+    
+    start = min(current_idx)
+    end = max(current_idx)
+    current_pat = price[current_idx]
+    return current_idx,current_pat,start,end  
+  
+def is_gartley(moves,err_allowed):  
+    
+    XA=moves[0]
+    AB=moves[1]
+    BC=moves[2]
+    CD=moves[3]
+    
+    AB_range = np.array([0.618 - err_allowed,0.618 + err_allowed])*abs(XA)
+    BC_range = np.array([0.382 - err_allowed,0.886 + err_allowed])*abs(AB)
+    CD_range = np.array([1.27 - err_allowed,1.618 + err_allowed])*abs(BC)
+        
+    
+    if XA>0 and AB<0 and BC>0 and CD<0:
+        
+        if AB_range[0]<abs(AB)<AB_range[1] and BC_range[0]<abs(BC)<BC_range[1] and CD_range[0]<abs(CD)<CD_range[1]:
+            return 1
+        else:
+            return np.NaN
+        
+    elif XA<0 and AB>0 and BC<0 and CD>0:
+        
+               
+        if AB_range[0]<abs(AB)<AB_range[1] and BC_range[0]<abs(BC)<BC_range[1] and CD_range[0]<abs(CD)<CD_range[1]:
+        
+            return -1
+        else:
+            return np.Nan
+    else:
+        return np.Nan
